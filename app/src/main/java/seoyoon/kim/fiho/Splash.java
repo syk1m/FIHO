a3e235a0e90849f2c8dd03a6deca9ced2d08af80
kunyoungPark <p3145@daum.net>
2022-04-12 17:21:47 +0900

    splash

---
+++ app/src/main/java/seoyoon/kim/fiho/Splash.java
@@ -0,0 +1,31 @@
+package seoyoon.kim.fiho;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), A_menu.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
